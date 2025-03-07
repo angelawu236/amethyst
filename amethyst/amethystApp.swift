3c5c12bd9f219c85b9c15613383fc0c517e0a791
angelawwu236 <zhu740513@gmail.com>
2025-03-06 18:37:03 -0800

    Initial Commit

---
+++ amethyst/amethystApp.swift
@@ -0,0 +1,17 @@
+//
+//  amethystApp.swift
+//  amethyst
+//
+//  Created by Angela Wu on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct amethystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ amethystTests/amethystTests.swift
@@ -0,0 +1,17 @@
+//
+//  amethystTests.swift
+//  amethystTests
+//
+//  Created by Angela Wu on 3/6/25.
+//
+
+import Testing
+@testable import amethyst
+
+struct amethystTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
