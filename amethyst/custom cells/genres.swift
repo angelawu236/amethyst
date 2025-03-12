babebe62cd797853c76eb8d78e65106a5e766c1c
angelawu236 <zhu740513@gmail.com>
2025-03-12 14:44:17 -0700

    edited genres

--- amethyst/custom cells/genres.swift
+++ amethyst/custom cells/genres.swift
@@ -5,22 +5,20 @@
 //  Created by Angela Wu on 3/11/25.
 //
 import SwiftUI
-
+//
 struct genres: View {
     @State private var title = titles.all
     
     var body: some View {
-        ScrollView(showsIndicators: false){
-                    VStack (alignment: .leading, spacing:30){
-                        ForEach(title){ title in
-                            cards(title: title)
-                        }
-                    }
-                                        
-            }
+        Text("hi")
+            .foregroundColor(colorManager.paleBlue)
+            .background(colorManager.darkBlue)
     }
 }
 
-#Preview {
-    genres()
+struct genres_Previews: PreviewProvider {
+    static var previews: some View {
+        cards(title: titles.shows())
+            .overlay(genres())
+    }
 }
