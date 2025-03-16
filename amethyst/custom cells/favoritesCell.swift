fb9f82f39df9e1e2b74b2f267368860213a36727
angelawu236 <zhu740513@gmail.com>
2025-03-16 00:35:02 -0700

    initial favorite cell

--- amethyst/custom cells/favoritesCell.swift
+++ amethyst/custom cells/favoritesCell.swift
@@ -11,15 +11,15 @@ struct favoritesCell: View {
     let favorite: favoriteTitles
     
     var body: some View {
-        HStack (spacing: 50){
+        HStack (spacing: 40){
             Image(favorite.favoriteImage)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 120, height: 100, alignment: .center)
+                .frame(width: 130, height: 100, alignment: .center)
             Image(favorite.favoriteImage)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 120, height: 100, alignment: .center)
+                .frame(width: 130, height: 100, alignment: .center)
         }
     }
 }
